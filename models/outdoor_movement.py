fc8ea593bfbf034fb472ba3a12eb80d7f9f28ac4
Kyle <kylewhite369@gmail.com>
2024-11-11 16:31:19 +1300

    Refactored files ensure they are Pep8 compliant

--- models/outdoor_movement.py
+++ models/outdoor_movement.py
@@ -1,5 +1,6 @@
 from models.player_movement import PlayerMovement
 
+
 class OutdoorMovement(PlayerMovement):
     def check_environment(self, next_tile):
         # Allow movement to outdoor or patio tiles, restrict others
@@ -7,14 +8,16 @@ class OutdoorMovement(PlayerMovement):
             return True
         elif next_tile.name == "Patio":
             return True
-        print("Movement from outdoor to indoor not allowed except through Patio.")
+        print("Movement from outdoor to indoor"
+              "not allowed except through Patio.")
         return False
 
     def draw_new_tile(self):
         # Draw the next outdoor tile
         next_tile = self.player.get_next_outdoor_tile()
         if next_tile:
-            print(self.player.localization["p_tile_drew"].format(tile=next_tile.name))
+            print(self.player.localization["p_tile_drew"]
+                  .format(tile=next_tile.name))
         else:
             print(self.player.localization["p_player_tile_no"])
         return next_tile
--- models/player_movement.py
+++ models/player_movement.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from enums.directions import Direction
 
+
 class PlayerMovement(ABC):
     def __init__(self, player, direction):
         self.player = player
         self.direction = direction
 
-    #Template Method
+    # Template Method
     def move(self):
         new_position = self.calculate_new_position()
         if new_position is None:
@@ -18,14 +19,12 @@ class PlayerMovement(ABC):
             if not self.prepare_next_tile(next_tile):
                 return False
             return self.update_position(new_position, next_tile)
-        
         next_tile = self.draw_new_tile()
         if not next_tile:
             return False
-        
         self.player.grid[new_position] = next_tile
         return self.update_position(new_position, next_tile)
-    
+
     def calculate_new_position(self):
         """Calculate new position based on direction."""
         x, y = self.player.position
@@ -33,19 +32,19 @@ class PlayerMovement(ABC):
             Direction.LEFT: (x - 1, y),
             Direction.UP: (x, y - 1),
             Direction.RIGHT: (x + 1, y),
-            Direction.DOWN: (x, y + 1), 
+            Direction.DOWN: (x, y + 1),
         }.get(self.direction)
-    
+
     def is_existing_tile(self, new_position):
         """Check if the tile already exists in the grid."""
         return new_position in self.player.grid
-    
+
     def prepare_next_tile(self, next_tile):
         """Check if the tile already exists in the grid."""
         if not self.align_exits(next_tile):
             return False
         return self.check_environment(next_tile)
-    
+
     def align_exits(self, next_tile):
         """Rotate tile to align exits."""
         opposite_index = Direction.opposite(self.direction).value
@@ -56,12 +55,12 @@ class PlayerMovement(ABC):
             next_tile.rotate()
         print(f"Failed to align exits for {next_tile.name}")
         return next_tile.walls[opposite_index] == 0
-    
+
     @abstractmethod
     def check_environment(self, next_tile):
         """Check environment-specific rules. To be overridden by subclasses."""
         raise NotImplementedError
-    
+
     def update_position(self, new_position, next_tile):
         """Update player position and move them to the new tile."""
         self.player.previous_tile = self.player.current_tile
@@ -69,14 +68,14 @@ class PlayerMovement(ABC):
         self.player.position = new_position
         print(
             self.player.localization["p_player_moved_from_to"].format(
-                direction = self.direction.name.lower(),
-                current_tile = self.player.current_tile.name,
+                direction=self.direction.name.lower(),
+                current_tile=self.player.current_tile.name,
             )
         )
         self.player.print_exits()
         self.player.visited_tiles.append(self.player.current_tile)
         return True
-    
+
     @abstractmethod
     def draw_new_tile(self):
         """Draw a new tile. To be overridden by subclasses."""
